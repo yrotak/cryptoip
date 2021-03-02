4fde8720c459c367405a1eccfb446f2c52a6c9fd
DrayNeur <50057580+DrayNeur@users.noreply.github.com>
2021-03-03 00:13:05 +0100

    angular

--- client/main.js
+++ client/main.js
@@ -1,31 +1,25 @@
-/**
- * @Date:   2020-12-05T23:36:55+01:00
- * @Last modified time: 2021-01-13T22:06:50+01:00
- * @License: MIT
- */
-
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 function createWindow () {
   const mainWindow = new BrowserWindow({
-    width: 550,
-    height: 300,
+    width: 1080,
+    height: 720,
     resizable: false,
     icon: __dirname + '/icon.ico',
     webPreferences: {
         nodeIntegration: true,
         enableRemoteModule: true
     }
-  })
-  mainWindow.removeMenu();
-  mainWindow.loadFile('index.html')
+  });
+  // mainWindow.removeMenu();
+  mainWindow.loadFile('index.html');
 }
 
-app.whenReady().then(() => {
-  createWindow()
+app.whenReady().then(function() {
+  createWindow();
 
   app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
   })
 })
 

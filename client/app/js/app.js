07f7cffaee91cb11e035f5e7a7bb069e3629ef77
DrayNeur <50057580+DrayNeur@users.noreply.github.com>
2021-03-03 16:08:00 +0100

    Signature & Encryption
    improvement made

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -1,6 +1,7 @@
 var socket = null;
 const cryptoip = angular.module("cryptoipApp", ["ngRoute"]);
 const fs = require('fs');
+const crypto = require('crypto')
 cryptoip.config(["$routeProvider", function ($routeProvider) {
   $routeProvider
     .when("/", { templateUrl: "app/html/home.html" })
@@ -24,7 +25,7 @@ cryptoip.controller("mainController", function ($scope) {
     $scope.passSentence = createPassSentence().replace(/[-]+/g, " ");
     $scope.setupClient = function (passSentence, usePassword, password) {
       if (usePassword) {
-        fs.writeFile("config.json", JSON.stringify({ encrypted: true, passsentence: CryptoJS.AES.encrypt(passSentence, password).toString() }), function (err) {
+        fs.writeFile("config.json", JSON.stringify({ encrypted: true, passsentence: encrypt(passSentence, password) }), function (err) {
           if (err) alert(err);
           window.location.reload();
         });
@@ -39,20 +40,70 @@ cryptoip.controller("mainController", function ($scope) {
   $scope.login = function (password) {
     fs.readFile("config.json", function (err, data) {
       if (err) alert(err);
-      $scope.passSentence = CryptoJS.AES.decrypt(JSON.parse(data).passsentence, password).toString(CryptoJS.enc.Utf8);
+      $scope.passSentence = decrypt(JSON.parse(data).passsentence, password);
       window.location.href = "#!/";
     });
   }
 });
+cryptoip.controller("homeController", function ($scope) {
+  $scope.connect = function(host) {
+    socket = io(host);
+    window.location.href = "#!/server/";
+  }
+});
+function encrypt(text, password) {
+  console.log(password.repeat(32).slice(0, 32));
+  var cipher = crypto.createCipheriv("aes-128-cbc", password.repeat(16).slice(0, 16), password.repeat(16).slice(0, 16))
+  var crypted = cipher.update(text, 'utf8', 'hex')
+  crypted += cipher.final('hex');
+  return crypted;
+}
+
+function decrypt(text, password) {
+  var decipher = crypto.createDecipheriv("aes-128-cbc", password.repeat(16).slice(0, 16), password.repeat(16).slice(0, 16))
+  var dec = decipher.update(text, 'hex', 'utf8')
+  dec += decipher.final('utf8');
+  return dec;
+}
 function createPassSentence() {
-  var names = ["bird", "clock", "boy", "plastic", "duck", "teacher", "old", "professor", "hamster", "dog", "keyboard", "screen", "background", "desk", "metal", "paper", "hacker", "pen", "rock", "dirt", "line", "string", "coin", "mouse", "rat", "lady", "woman", "man", "past", "futur", "drive", "car", "chair", "hashtag", "rule", "notification", "bell", "sound", "stick", "letter", "sentence", "clay", "gravel", "security", "nice", "good", "drawing", "cow", "signature", "dust", "finest", "crazy", "sad", "wood", "lamp", "cable"];
+  var names = ["bird", "clock", "boy", "plastic", "duck", "teacher", "old", "professor", "hamster", "dog", "keyboard", "screen", "background", "desk", "metal", "paper", "hacker", "pen", "rock", "dirt", "line", "string", "coin", "mouse", "rat", "lady", "woman", "man", "past", "futur", "drive", "car", "chair", "hashtag", "rule", "notification", "bell", "sound", "stick", "letter", "sentence", "clay", "gravel", "security", "nice", "good", "drawing", "cow", "signature", "dust", "finest", "crazy", "sad", "wood", "lamp", "cable", "phone", "number", "matrix", "vector", "plug", "port", "house", "pepper", "potato", "microphone", "love", "flower", "plant", "fiber", "fruit", "destination", "wall"];
   var finalSentence = "";
   for (var i = 0; i < 10; i++) {
     finalSentence += names[Math.floor(Math.random() * Math.floor(names.length))] + "-";
   }
   return finalSentence.slice(0, -1);
 }
-
+function createKeyPair(passSentence) {
+  const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
+    modulusLength: 1024 * 2,
+    publicKeyEncoding: {
+      type: 'spki',
+      format: 'pem'
+    },
+    privateKeyEncoding: {
+      type: 'pkcs8',
+      format: 'pem',
+      cipher: 'aes-256-cbc',
+      passphrase: passSentence
+    }
+  });
+  return {
+    privateKey: privateKey,
+    publicKey: publicKey
+  };
+}
+function sign(message, privateKey, passSentence) {
+  const sign = crypto.createSign('SHA256');
+  sign.write(message);
+  sign.end();
+  return sign.sign({ key: privateKey, passphrase: passSentence }, 'hex');
+}
+function verify(message, signature, publicKey) {
+  const verify = crypto.createVerify('SHA256');
+  verify.write(message);
+  verify.end();
+  return verify.verify(publicKey, signature, 'hex');
+}
 var iv = new Uint8Array();
 var key = new Uint8Array();
 var xorKey = "";

32567fba1f2a7b233182353757b0aaf7d0db890b
DrayNeur <50057580+DrayNeur@users.noreply.github.com>
2021-06-15 20:15:15 +0200

    update

--- clientReact/src/js/components/Server.js
+++ clientReact/src/js/components/Server.js
@@ -1,13 +1,73 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 var callThread = null;
 const Server = (props) => {
     const [muted, setMuted] = useState(false);
     const [inCall, setInCall] = useState(false);
-    console.log(props.channels);
+    const [serverInfos, setServerInfos] = useState({});
+    const [channelsServer, setChannels] = useState([]);
+    const [clientCallList, setClientCallList] = useState([]);
+    const [currentChannel, setCurrentChannel] = useState('main');
+    useEffect(() => {
+        props.socket.on("infos", function (infos, secureKey) {
+            var infosDecrypt = JSON.parse(electron.utilApi.dec(infos, secureKey));
+            setServerInfos({
+                endpoint: props.serverHost,
+                motd: infosDecrypt.motd,
+                mainKey: infosDecrypt.mainKey
+            });
+        });
+        props.socket.on("clientList", function (clientList, secureKey) {
+            var clientListDecrypt = JSON.parse(electron.utilApi.dec(clientList, secureKey));
+            setChannels([{ name: "main", socketId: "none", messages: [], publicKey: [], clientKey: serverInfos.mainKey, unread: 0 }]);
+            clientListDecrypt.forEach((client) => {
+                setChannels(channelsServer => [...channelsServer, { name: client.username, socketId: client.socketId, messages: [], publicKey: client.publicKey, clientKey: client.clientKey, unread: 0 }]);
+            });
+        });
+        props.socket.on("kick", function (reason, secureKey) {
+            props.socket.disconnect();
+            setConnected(false);
+            alert(electron.utilApi.dec(reason, secureKey));
+        });
+        props.socket.on("message", function (messageData, secureKey) {
+            var messageDataDecrypt = JSON.parse(electron.utilApi.dec(messageData, secureKey));
+
+            var tofind = messageDataDecrypt.isMain ? "main" : messageDataDecrypt.author;
+            console.log(channelsServer);
+            setChannels(channelsServer => channelsServer.map(channel => {
+                if (channel.name == tofind) {
+                    return {
+                        ...channel, messages: [...channel.messages, {
+                            author: messageDataDecrypt.author,
+                            content: electron.utilApi.dec(messageDataDecrypt.message, channel.clientKey),
+                            signature: messageDataDecrypt.signature,
+                            isMain: messageDataDecrypt.isMain,
+                            checked: messageDataDecrypt.isMain ?
+                                electron.utilApi.verifySign(electron.utilApi.dec(messageDataDecrypt.message, channel.clientKey), messageDataDecrypt.signature, messageDataDecrypt.publicKey) :
+                                electron.utilApi.verifySign(electron.utilApi.dec(messageDataDecrypt.message, channel.clientKey), messageDataDecrypt.signature, channel.publicKey)
+                        }]
+                    };
+                } else {
+                    return channel;
+                }
+            }))
+        });
+        props.socket.on("joinedcall", () => {
+            var audio = new Audio('./connected.wav');
+            audio.play();
+        });
+        props.socket.on("disconnectcall", () => {
+            var audio = new Audio('./disconnect.wav');
+            audio.play();
+        });
+        props.socket.on("callList", (callListReceive, secureKey) => {
+            var callListDecrypt = JSON.parse(electron.utilApi.dec(callListReceive, secureKey));
+            setClientCallList(callListDecrypt);
+        });
+    }, []);
     return (
-        <div className="server">
+        <div className="server" onLoad={() => { console.log("ttt"); }}>
             <div className="server-info">
-                <h2 className="title host-title">{props.serverInfos.endpoint}</h2>
+                <h2 className="title host-title">{serverInfos.endpoint}</h2>
                 <button className="form-button disconnect-button">Disconnect</button>
                 <div className="call-buttons">
                     <button className={"form-button-outline button-call " + (muted && inCall ? 'hidden' : 'show')} onClick={() => {
@@ -62,7 +122,7 @@ const Server = (props) => {
                     });
                 }}>call</button>
 
-                <h5 className="title motd">{props.serverInfos.motd}</h5>
+                <h5 className="title motd">{serverInfos.motd}</h5>
             </div>
 
             <div className={"callPanel " + (inCall ? 'show' : 'hidden')}>
@@ -70,8 +130,8 @@ const Server = (props) => {
                 <hr />
                 <div className="call-list">
                     {
-                        props.callList.map((client) => (
-                            <a className="user-call" key={client.socketId}>{props.channels[props.channels.findIndex(p => p.socketId == client.socketId)].name} <i className={"fas fa-microphone-slash " + (client.muted ? 'show' : 'hidden')}></i></a>
+                        clientCallList.map((client) => (
+                            <a className="user-call" key={client.socketId}>{channelsServer[channelsServer.findIndex(p => p.socketId == client.socketId)].name} <i className={"fas fa-microphone-slash " + (client.muted ? 'show' : 'hidden')}></i></a>
                         ))
                     }
                 </div>
@@ -79,9 +139,9 @@ const Server = (props) => {
 
             <div className="users">
                 {
-                    props.channels.map((channel) => (
+                    channelsServer.map((channel) => (
                         <a key={channel.name} className="channel-button" onClick={() => {
-                            props.currentChannelHandle(channel.name)
+                            setCurrentChannel(channel.name)
                         }}>{(channel.name == "main" ? "#" : "@") + channel.name}<p className="notif">{channel.unread}</p></a>
                     ))
                 }
@@ -92,8 +152,8 @@ const Server = (props) => {
                 <h4 className="title currentChannel-name">-</h4>
                 <div className={"messages " + (inCall ? 'incall' : 'show')}>
                     {
-                        props.channels.findIndex(p => p.name == props.currentChannel) != -1 ? (
-                            props.channels[props.channels.findIndex(p => p.name == props.currentChannel)].messages.map((message) => (
+                        channelsServer.findIndex(p => p.name == currentChannel) != -1 ? (
+                            channelsServer[channelsServer.findIndex(p => p.name == currentChannel)].messages.map((message) => (
                                 <p>{JSON.stringify(message)}</p>
                             ))
                         ) : (
@@ -102,7 +162,7 @@ const Server = (props) => {
                     }
                 </div>
                 <form className="sendMessage">
-                    <input className={"input message-send " + (inCall ? 'incall-input' : 'show')} type="text" placeholder={"Message to " + props.currentChannel}></input>
+                    <input className={"input message-send " + (inCall ? 'incall-input' : 'show')} type="text" placeholder={"Message to " + currentChannel}></input>
                 </form>
             </div>
         </div>
--- clientReact/src/js/pages/Main.js
+++ clientReact/src/js/pages/Main.js
@@ -6,7 +6,7 @@ var socket = null;
 
 function MainPages(props) {
     if (props.connected) {
-        return <Server channels={props.channels} serverInfos={props.serverInfos} socket={socket} callList={props.callList} currentChannel={props.currentChannel} currentChannelHandle={props.currentChannelHandle} />;
+        return <Server serverInfos={props.serverInfos} socket={socket} serverHost={props.serverHost} />;
     } else {
         return <ConnectToServer userInfos={props.userInfos} connectToServerHandle={props.connectToServerHandle} />;
     }
@@ -24,87 +24,24 @@ const Main = (props) => {
     const [connected, setConnected] = useState(false);
     const [clientKey, setClientKey] = useState('');
     const [keyPair, setKeyPair] = useState({});
-    const [serverInfos, setServerInfos] = useState({});
-    const [channels, setChannels] = useState([]);
-    const [clientCallList, setClientCallList] = useState([]);
-    const [currentChannel, setCurrentChannel] = useState('main');
+    const [serverHost, setServerHost] = useState('');
+
 
     const connectToServer = (e, server, username) => {
         e.preventDefault();
         setClientKey(randomString(16));
         setKeyPair(electron.utilApi.createKeys(props.userInfos.passSentence))
         socket = io("http://" + server);
+        setServerHost(server);
         socket.on("connect", () => {
             setConnected(true);
             var secureKey = randomString(16);
             socket.emit("connection", electron.utilApi.enc(JSON.stringify({ username: username, keyHash: props.userInfos.keyHash, publicKey: keyPair.publicKey, clientKey: clientKey }), secureKey), secureKey);
-            socket.on("infos", function (infos, secureKey) {
-                var infosDecrypt = JSON.parse(electron.utilApi.dec(infos, secureKey));
-                setServerInfos({
-                    endpoint: server,
-                    motd: infosDecrypt.motd,
-                    mainKey: infosDecrypt.mainKey
-                });
-            });
-            socket.on("clientList", function (clientList, secureKey) {
-                var clientListDecrypt = JSON.parse(electron.utilApi.dec(clientList, secureKey));
-                setChannels([{ name: "main", socketId: "none", messages: [], publicKey: [], clientKey: serverInfos.mainKey, unread: 0 }]);
-                clientListDecrypt.forEach((client) => {
-                    setChannels(channels => [...channels, { name: client.username, socketId: client.socketId, messages: [], publicKey: client.publicKey, clientKey: client.clientKey, unread: 0 }]);
-                });
-            });
-            socket.on("kick", function (reason, secureKey) {
-                socket.disconnect();
-                setConnected(false);
-                alert(electron.utilApi.dec(reason, secureKey));
-            });
-            socket.on("message", function (messageData, secureKey) {
-                var messageDataDecrypt = JSON.parse(electron.utilApi.dec(messageData, secureKey));
-
-                var tofind = messageDataDecrypt.isMain ? "main" : messageDataDecrypt.author;
-                console.log(channels);
-                setChannels(channels => channels.map(channel => {
-                    if(channel.name == tofind) {
-                        return {...channel, messages: [...channel.messages, { author: messageDataDecrypt.author, content: electron.utilApi.dec(messageDataDecrypt.message, channel.clientKey), signature: messageDataDecrypt.signature, isMain: messageDataDecrypt.isMain, checked: messageDataDecrypt.isMain ? electron.utilApi.verifySign(electron.utilApi.dec(messageDataDecrypt.message, channel.clientKey), messageDataDecrypt.signature, messageDataDecrypt.publicKey) : electron.utilApi.verifySign(electron.utilApi.dec(messageDataDecrypt.message, channel.clientKey), messageDataDecrypt.signature, channel.publicKey) }]};
-                    } else {
-                        return channel;
-                    }
-                }))
-                // var channelsTemp = channels;
-                // if (channelsTemp.findIndex(p => p.name == tofind) != -1) {
-                //     channelsTemp[channelsTemp.findIndex(p => p.name == tofind)].messages.push({ author: messageDataDecrypt.author, content: electron.utilApi.dec(messageDataDecrypt.message, channelsTemp[channelsTemp.findIndex(p => p.name == tofind)].clientKey), signature: messageDataDecrypt.signature, isMain: messageDataDecrypt.isMain, checked: messageDataDecrypt.isMain ? electron.utilApi.verifySign(electron.utilApi.dec(messageDataDecrypt.message, channelsTemp[channelsTemp.findIndex(p => p.name == tofind)].clientKey), messageDataDecrypt.signature, messageDataDecrypt.publicKey) : electron.utilApi.verifySign(electron.utilApi.dec(messageDataDecrypt.message, channelsTemp[channelsTemp.findIndex(p => p.name == tofind)].clientKey), messageDataDecrypt.signature, channelsTemp[channelsTemp.findIndex(p => p.name == tofind)].publicKey) });
-                //     if (currentChannel != channels[channels.findIndex(p => p.name == tofind)].name) {
-                //         channelsTemp[channelsTemp.findIndex(p => p.name == tofind)].unread++;
-                //     }
-                // }
-                // setChannels(channelsTemp);
-                // currentChannel.messages.forEach((message) => {
-                //     var color = message.checked ? '#19b019' : '#b02819';
-                //     var icon = message.checked ? 'check-square' : 'times';
-                //     var checkResult = message.checked ? 'valid' : 'invalid';
-                //     var messageText = $('<p class="text-normal"></p>').text(message.content);
-                //     var messageContent = $('<div class="message-content"></div>').append(messageText).append($('<div class="signature-check"><i style="color: ' + color + ';" class="fas fa-' + icon + '"></i><p class="hover"><strong>Signature:</strong> ' + message.signature + ' (' + checkResult + ')</p></div>'));
-                //     var message = $('<li class="message"><h5 class="title">' + message.author.replace(/<(|\/|[^>\/bi]|\/[^>bi]|[^\/>][^>]+|\/[^>][^>]+)>/g, '') + '</h5></li>').append(messageContent);
-                //     $(".messages").append(message);
-                // });
-            });
-            socket.on("joinedcall", () => {
-                var audio = new Audio('./connected.wav');
-                audio.play();
-            });
-            socket.on("disconnectcall", () => {
-                var audio = new Audio('./disconnect.wav');
-                audio.play();
-            });
-            socket.on("callList", (callListReceive, secureKey) => {
-                var callListDecrypt = JSON.parse(electron.utilApi.dec(callListReceive, secureKey));
-                setClientCallList(callListDecrypt);
-            });
         });
     };
     return (
         <div className="page main">
-            <MainPages connected={connected} userInfos={props.userInfos} connectToServerHandle={connectToServer} channels={channels} serverInfos={serverInfos} socket={socket} callList={clientCallList} currentChannel={currentChannel} currentChannelHandle={setCurrentChannel} />
+            <MainPages connected={connected} userInfos={props.userInfos} connectToServerHandle={connectToServer} socket={socket} serverHost={serverHost}/>
         </div>
     );
 };
